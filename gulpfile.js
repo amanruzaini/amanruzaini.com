e464825a79526cef1cd3379b099623e0755b316e
Aman Ruzaini <ruzaini_ahmat@astro.com.my>
2020-01-19 12:02:54 +0800

    first init

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function() {
+  return gulp
+    .src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('watch', () => {
+  gulp.watch('sass/**/*.scss', done => {
+    gulp.series(['sass'])(done);
+  });
+});
